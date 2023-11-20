8a3e195eabb9986a60e33839b1fed0e8ebd6bbed
NagyGergely <nagygergely526@gmail.com>
2023-11-20 14:37:28 +0100

    Morgan naplózó + Middlewarek használata

--- routes/trainings.js
+++ routes/trainings.js
@@ -1,19 +1,14 @@
-const express = require('express')
-const router = express.Router()
-router.get("/", (req, res) => {
-    res.status(200).json({ success: true, msg: "Show all courses" });
-});
-router.get("/:id", (req, res) => {
-    res.status(200).json({ success: true, msg: `Get course ${req.params.id}` });
-});
-router.post("/", (req, res) => {
-    res.status(200).json({ success: true, msg: "Create new course" });
-});
-router.put("/:id", (req, res) => {
-    res.status(200).json({ success: true, msg: `Update course ${req.params.id}` });
-});
-router.delete("/:id", (req, res) => {
-    res.status(200).json({ success: true, msg: `Delete course ${req.params.id}` });
-});
+const express = require("express");
+const {
+    getTraining,
+    getTrainings,
+    createTraining,
+    updateTraining,
+    deleteTraining,
+} = require("../controllers/training");
 
-module.exports = router
+const router = express.Router();
+router.route("/").get(getTrainings).post(createTraining);
+router.route("/:id").get(getTraining).put(updateTraining).delete(deleteTraining);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express")
 require("dotenv").config()
 const trainings = require("./routes/trainings")
+const logger = require('./middleware/logger')
+const morgan = require('morgan')
 
 const app = express()
+
+app.use(morgan('dev'))
 
 app.use("/api/trainings", trainings)
 
