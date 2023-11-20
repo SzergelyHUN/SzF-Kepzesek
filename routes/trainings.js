66bc5186765c135f93cccf1e77fa08ab27f7e66e
NagyGergely <nagygergely526@gmail.com>
2023-11-20 14:10:36 +0100

    javított trainings.js

--- routes/trainings.js
+++ routes/trainings.js
@@ -1,26 +1,19 @@
-const express = require("express")
-const app = express()
+const express = require('express')
 const router = express.Router()
+router.get("/", (req, res) => {
+    res.status(200).json({ success: true, msg: "Show all courses" });
+});
+router.get("/:id", (req, res) => {
+    res.status(200).json({ success: true, msg: `Get course ${req.params.id}` });
+});
+router.post("/", (req, res) => {
+    res.status(200).json({ success: true, msg: "Create new course" });
+});
+router.put("/:id", (req, res) => {
+    res.status(200).json({ success: true, msg: `Update course ${req.params.id}` });
+});
+router.delete("/:id", (req, res) => {
+    res.status(200).json({ success: true, msg: `Delete course ${req.params.id}` });
+});
 
-
-app.get('/api/trainings', (req, res) => {
-        res.status(200).json({ success: true, msg: 'Show all trainings '})
-    })
-    
-    app.get('/api/trainings/:id', (req, res) => {
-        res.status(200).json({ success: true, msg: `Get training ${req.params.id}`})
-    })
-    
-    app.post('/api/trainings', (req, res) => {
-        res.status(200).json({ success: true, msg: 'Create new training'})
-    })
-    
-    app.put('/api/trainings/:id', (req, res) => {
-        res.status(200).json({ success: true, msg: `Update training ${req.params.id}`})
-    })
-    
-    app.delete('/api/trainings/:id', (req, res) => {
-        res.status(200).json({ success: true, msg: `Delete training ${req.params.id}`}) 
-    })
-    
 module.exports = router
